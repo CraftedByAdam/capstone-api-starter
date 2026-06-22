f2db881e1e97832ec54a8a8dc216258f54fa6d42
Your Name <youremail@example.com>
2026-06-22 16:44:29 -0400

    I added the '@PostMapping("/products/{productId}")' annotation and the ' @PreAuthorize("isAuthenticated()")' annotation. I then make a method called 'updatedCart' and used the principle parameter as well as a '@PathVariable' for my productId, this method has a return type for 'ResponseEntity<ShoppingCart>'. I do the same that was done in the 'getCart' method but this time I create a variable for my 'ShoppingCart' and call it 'update' and then call my method from the 'shoppingCartService' and pass the 'userId' and 'productId' ito it, then I return the 'ResponseEntity' with the status code 'CREATED' and update the body.

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,11 +1,9 @@
 package org.yearup.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.User;
 import org.yearup.service.ShoppingCartService;
@@ -42,8 +40,17 @@ public class ShoppingCartController
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15  (15 is the productId to be added)
-    // return the updated cart with status 201 Created
+    @PostMapping("/products/{productId}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<ShoppingCart> updatedCart(Principal principal, @PathVariable int productId) {
+        // return the updated cart with status 201 Created
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
 
+        ShoppingCart updated = shoppingCartService.updateCart(userId, productId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(updated);
+    }
 
     // add a PUT method to update an existing product in the cart - the url should be
     // https://localhost:8080/cart/products/15  (15 is the productId to be updated)
