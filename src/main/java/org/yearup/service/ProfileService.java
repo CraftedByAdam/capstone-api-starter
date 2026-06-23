b2849627b03875b66eb4878c9c58ce4db89701d4
Your Name <youremail@example.com>
2026-06-23 12:51:41 -0400

    I added an 'update' method that takes a 'userId' and a 'Profile' object. I used the repository to find the actually profile first then I used setters to "set" every field from the new profile object to the existing one. I save the updated profile and return it.

--- src/main/java/org/yearup/service/ProfileService.java
+++ src/main/java/org/yearup/service/ProfileService.java
@@ -1,6 +1,7 @@
 package org.yearup.service;
 
 import org.springframework.stereotype.Service;
+import org.yearup.models.Product;
 import org.yearup.models.Profile;
 import org.yearup.repository.ProfileRepository;
 
@@ -22,5 +23,20 @@ public class ProfileService
     public Profile getById(int userId)
     {
         return profileRepository.findById(userId).orElse(null);
+    }
+
+    public Profile update(int userId, Profile profile)
+    {
+        Profile existing = profileRepository.findById(userId).orElseThrow();
+        existing.setFirstName(profile.getFirstName());
+        existing.setLastName(profile.getLastName());
+        existing.setPhone(profile.getPhone());
+        existing.setEmail(profile.getEmail());
+        existing.setAddress(profile.getAddress());
+        existing.setCity(profile.getCity());
+        existing.setState(profile.getState());
+        existing.setZip(profile.getZip());
+
+        return profileRepository.save(existing);
     }
 }
