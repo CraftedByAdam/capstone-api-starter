b4592b881c550e6fdd5af5657218103af4d44237
Your Name <youremail@example.com>
2026-06-23 09:39:02 -0400

    I created a 'ProfileController' class and added the '@RestController', '@RequestMapping("/profile")', and '@CrossOrigin' annotations. I made a constructor to inject the 'ProfileService' and 'UserService'. Then I made a 'getProfile' method and added '@GetMapping' and '@PreAuthorize("isAuthenticated")' annotations and used the 'Principle' object to find out who is logged in so I could get their 'userId' and return their specific profile from the service.

---
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -0,0 +1,38 @@
+package org.yearup.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+import org.yearup.service.ProfileService;
+import org.yearup.service.UserService;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/profile")
+@CrossOrigin
+public class ProfileController {
+
+    public ProfileService profileService;
+    private UserService userService;
+
+    public ProfileController(ProfileService profileService, UserService userService) {
+        this.profileService = profileService;
+        this.userService = userService;
+    }
+
+    @GetMapping
+    @PreAuthorize("isAuthenticated")
+    public Profile getProfile(Principal principal) {
+
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
+
+        return profileService.getById(userId);
+    }
+}
