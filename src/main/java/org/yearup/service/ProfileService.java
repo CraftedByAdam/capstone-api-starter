e5a84c1856f3833ecb95f469b4d6f9ccdb8543a1
Your Name <youremail@example.com>
2026-06-25 14:52:18 -0400

    I added comments that describe the work I did in a high level picture.

--- src/main/java/org/yearup/service/ProfileService.java
+++ src/main/java/org/yearup/service/ProfileService.java
@@ -1,7 +1,6 @@
 package org.yearup.service;
 
 import org.springframework.stereotype.Service;
-import org.yearup.models.Product;
 import org.yearup.models.Profile;
 import org.yearup.repository.ProfileRepository;
 
@@ -20,14 +19,17 @@ public class ProfileService
         return profileRepository.save(profile);
     }
 
+    //Looks up a profile in the repo using the userId.
     public Profile getById(int userId)
-    {
+    {                            //added orElse(null) so the controller can handle it if not found.
         return profileRepository.findById(userId).orElse(null);
     }
 
     public Profile update(int userId, Profile profile)
     {
+        //Finds the original first
         Profile existing = profileRepository.findById(userId).orElseThrow();
+        //Overwrites each field with the new information before saving back to the database.
         existing.setFirstName(profile.getFirstName());
         existing.setLastName(profile.getLastName());
         existing.setPhone(profile.getPhone());
